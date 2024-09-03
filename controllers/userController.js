cb8ef1f2995f27050e24e838108a7fa506e5d24a
unknown <Bgoodluck>
2024-09-03 01:25:03 +0100

    verify

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,71 @@
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const validator = require("validator");
+// const userSchema = require("../models/user");
+// const transporter = require("../config/nodemailer");  // Import transporter
+// const { v4 : uuidv4 } = require("uuid");       // Import uuid for generating tokens
+
+
+// const createToken = (userId) => {
+//   return jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
+// };
+
+// exports.registerUser = async (req, res) => {
+//   const { UserName, email, password } = req.body;
+
+//   try {
+//     const exist = await userSchema.findOne({ email });
+//     if (exist) {
+//       return res.json({ success: false, message: "User already exists" });
+//     }
+
+//     if (!validator.isEmail(email)) {
+//       return res.json({ success: false, message: "Please provide a valid email" });
+//     }
+    
+
+//     if (password.length < 8) {
+//       return res.json({ success: false, message: "Please enter a strong password" });
+//     }
+
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     const verificationToken = uuidv4(); 
+
+//     const newUser = new userSchema({
+//       UserName,
+//       email,
+//       password: hashedPassword,
+//       verificationToken, 
+//       isVerified: false
+//     });
+//     const User = await newUser.save();
+
+  
+//     const token = createToken(User._id);
+
+//     const verificationUrl = `${process.env.FRONTEND_URL}/api/email/verify/${verificationToken}`;
+//         const mailOptions = {
+//             from: process.env.EMAIL_USER,
+//             to: email,
+//             subject: 'Email Verification',
+//             text: `Please verify your email by clicking the link: ${verificationUrl}`
+//         };
+
+//         await transporter.sendMail(mailOptions);
+
+//       res.json({ success: true, message: "Registration Successful", token, user: User });
+
+//   } catch (error) {
+//     console.log(error);
+//     res.json({ success: false, message: "Error" });
+//   }
+// };
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const userSchema = require("../models/user");
-const transporter = require("../config/nodemailer");  // Import transporter
-const { v4 : uuidv4 } = require("uuid");       // Import uuid for generating tokens
-
 
 const createToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" });
@@ -22,7 +83,6 @@ exports.registerUser = async (req, res) => {
     if (!validator.isEmail(email)) {
       return res.json({ success: false, message: "Please provide a valid email" });
     }
-    
 
     if (password.length < 8) {
       return res.json({ success: false, message: "Please enter a strong password" });
@@ -31,37 +91,26 @@ exports.registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const verificationToken = uuidv4(); 
-
     const newUser = new userSchema({
       UserName,
       email,
       password: hashedPassword,
-      verificationToken, 
-      isVerified: false
+      isVerified: true // Assume user is verified since email verification is removed
     });
-    const User = await newUser.save();
 
-  
+    const User = await newUser.save();
     const token = createToken(User._id);
 
-    const verificationUrl = `${process.env.FRONTEND_URL}/api/email/verify/${verificationToken}`;
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: 'Email Verification',
-            text: `Please verify your email by clicking the link: ${verificationUrl}`
-        };
-
-        await transporter.sendMail(mailOptions);
-
-      res.json({ success: true, message: "Registration Successful", token, user: User });
+    res.json({ success: true, message: "Registration Successful", token, user: User });
 
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
   }
 };
+
+
+
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
